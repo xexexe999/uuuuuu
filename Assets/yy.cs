03fe3fdbaf24fd726c1d78fd45f28e64d035a2cf
aran <aas711@nyu.edu>
2015-09-29 10:22:00 -0800

    ff

---
+++ Assets/yy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class yy : MonoBehaviour
+{
+    //the factor used to slow down time  
+    private float slowFactor = 4f;
+    //the new time scale  
+    private float newTimeScale;
+
+    // Called when this script starts  
+    void Start()
+    {
+        //calculate the new time scale  
+        newTimeScale = Time.timeScale / slowFactor;
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        
+            //if the game is running normally  
+            if (Time.timeScale == 1.0f)
+            {
+                //assign the 'newTimeScale' to the current 'timeScale'  
+                Time.timeScale = newTimeScale;
+                //proportionally reduce the 'fixedDeltaTime', so that the Rigidbody simulation can react correctly  
+                Time.fixedDeltaTime = Time.fixedDeltaTime / slowFactor;
+                //The maximum amount of time of a single frame  
+                Time.maximumDeltaTime = Time.maximumDeltaTime / slowFactor;
+            }
+    }
+}
